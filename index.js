998d77d5b9c589367bfbe9d15e58a94b169cfb81
Dundgren <danne.l.97@hotmail.com>
2021-11-16 10:52:04 +0100

    Fixed lint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // const express = require("express");
 
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const { graphqlHTTP } = require("express-graphql");
 const connectToDatabase = require("./db/database");
 const read = require("./src/read");
